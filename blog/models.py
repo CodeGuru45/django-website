e25cf33cdc3e54074832864d4eabd6116f54a507
CodeGuru45 <eyeque27@gmail.com>
2020-03-20 14:37:01 -0400

    added sort by rating functionality

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation
+from star_ratings.models import Rating
 
 # Create your models here.
 class Post(models.Model):
-    content = models.TextField()
     music_url = models.URLField(default='') 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    ratings = GenericRelation(Rating, related_query_name='posts')
+    
 
 class Comment(models.Model):
     title = models.CharField(max_length=100)
@@ -16,6 +19,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.title
-
 
 
