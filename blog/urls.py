16de63bb80c7b560ae394aa52d92757a317a7a94
CodeGuru45 <eyeque27@gmail.com>
2020-03-26 21:30:09 -0400

    updated url paths

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from blog import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('user/<int:pk>', views.delete, name='post-delete'),
+    path('user/<str:username>', views.user_posts, name='posts-user'),
+    path('post/new/', views.create, name='post-create'),
 ]  
