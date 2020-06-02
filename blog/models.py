7689de13f1e3901d19ac268d3fea67c553b26877
Theodore Tasker <eyeque27@gmail.com>
2020-06-02 00:03:07 -0500

    deleted comment model

--- blog/models.py
+++ blog/models.py
@@ -9,13 +9,6 @@ class Post(models.Model):
     music_url = models.URLField(default='') 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     ratings = GenericRelation(Rating, related_query_name='posts')
-    
-
-class Comment(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
