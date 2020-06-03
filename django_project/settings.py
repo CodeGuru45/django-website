20cbefa8cf1541ea693ab6e7047970bb6c7d2ce6
CodeGuru45 <eyeque27@gmail.com>
2020-06-03 16:20:52 -0400

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myawesomedjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['spotifyshareapp.herokuapp.com']
 
 
 # Application definition
